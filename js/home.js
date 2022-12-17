036214ed3d8f8453ab53801ada43f85d5279bbde
Romain DELIMAL <rom.delimal@gmail.com>
2022-12-18 00:54:29 +0100

    ContiuationGrid

---
+++ js/home.js
@@ -0,0 +1,14 @@
+// const square8 = document.getElementById("s8");
+// const buttonwelcome = document.querySelector("#buttonwelcome");
+
+// buttonwelcome.addEventListener("click", (event) => {
+//   console.log("buttonwelcome");
+// });
+
+function buttonwelcome() {
+  console.log("buttonwelcome");
+  const element = document.getElementById('s8')
+  element.classList.remove("s8");
+  element.offsetWidth
+  element.classList.add("s8");
+}
